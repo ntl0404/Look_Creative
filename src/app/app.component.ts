0e3689d2585bbf9adf231512c03d22a7ef4a7735
NguyenLoc0404 <1427023@hcmut.edu.vn>
2021-09-05 22:31:30 +0700

    initial test intern

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'GuardSignature';
+  collapsed = true;
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+}
